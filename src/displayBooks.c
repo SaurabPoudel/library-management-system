c4785d95e4b90701feac9d04d2cb34287dd4b10a
SMAK <sashminadhikari@gmail.com>
2023-08-10 17:34:50 +0545

    Fixed Display Books' Alignment

--- src/displayBooks.c
+++ src/displayBooks.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
+
 
 #define ANSI_COLOR_GREEN "\x1b[32m"
 #define ANSI_COLOR_RED "\x1b[31m"
@@ -15,6 +17,17 @@ struct Book
     bool isBorrow;
 };
 
+void charFiller(char info[], int spaces) {
+    int currentLength = strlen(info);
+    int fillNumber = spaces - currentLength;
+    
+    if (fillNumber > 0) {
+        for (int i = 0; i < fillNumber; i++) {
+            strcat(info, " ");
+        }
+    }
+}
+
 void displayBooks()
 {
     FILE *file = fopen("./data/books.csv", "r");
@@ -28,13 +41,16 @@ void displayBooks()
     system("cls");
     printf(ANSI_COLOR_GREEN "-----------Library Management System-----------\n");
     printf("\n------------------Display Books------------------\n" ANSI_COLOR_RESET);
-    printf("\nID\tTitle\t\t\tAuthor\t\t\tYear\t\tBorrowed\n");
+    printf("\nID\tTitle\t\t\t\t\tAuthor\t\t\t\t\tYear\t\tBorrowed\n");
     unsigned int borrowInt;
     while (fscanf(file, "%d,%[^,],%[^,],%u,%u\n", &book.id, book.title, book.author, &book.year, &borrowInt) != EOF)
     {
+        charFiller(book.title, 20);
+        charFiller(book.author, 20);
         book.isBorrow = borrowInt ? true : false;
         printf("%d\t%s\t\t\t%s\t\t\t%d\t\t%s\n", book.id, book.title, book.author, book.year, book.isBorrow ? "Yes" : "No");
     }
     printf("\n");
     fclose(file);
+
 }
